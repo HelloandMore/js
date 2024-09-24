35c0fb5b5a5a7bd9e2f65df6f688de238171f6f2
konmil <milan.kondor06@gmail.com>
2024-09-24 14:21:58 +0200

    2024. 09. 24. - auto git push, ma mar sokadik push

---
+++ auto_git.cmd
@@ -0,0 +1,20 @@
+@echo off
+set /p selection="1. Push vagy 2. Pull > "
+
+if /i %selection%=="push" or %selection%=="pus" or %selection%=="1" (
+    git add .
+    git status
+    echo.
+    set /p commit="Add meg a commit messaget > "
+    git commit -m "%date% - %commit%"
+    git push
+)
+else /i if %selection%=="pull" or %selection%=="pul" or %selection=="2" (
+    git pull
+)
+
+if not %errorlevel%==0 (
+    net helpmsg %errorlevel%
+    timeout 5 /nobreak > nul
+    exit /b %errorlevel%
+)
