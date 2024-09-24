72a55a456bc5fc76685dccce2dfbb5d002b4b5e8
konmil <milan.kondor06@gmail.com>
2024-09-24 14:22:53 +0200

    2024. 09. 24. - auto git mukszik as intended

--- auto_git.cmd
+++ auto_git.cmd
@@ -8,11 +8,14 @@ if /i %selection%=="push" or %selection%=="pus" or %selection%=="1" (
     set /p commit="Add meg a commit messaget > "
     git commit -m "%date% - %commit%"
     git push
+    goto :end
 )
 else /i if %selection%=="pull" or %selection%=="pul" or %selection=="2" (
     git pull
+    goto :end
 )
 
+:end
 if not %errorlevel%==0 (
     net helpmsg %errorlevel%
     timeout 5 /nobreak > nul
