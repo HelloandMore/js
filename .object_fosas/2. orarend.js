ebcd31c2b3d2f8f90811609ef30bd8a44acec7b4
konmil <milan.kondor06@gmail.com>
2024-12-03 13:45:14 +0100

    2024. 12. 03. -

--- .object_fosas/2. orarend.js
+++ .object_fosas/2. orarend.js
@@ -1,37 +1,31 @@
-import input from './input.js';
+import input from './input.js' 
 
-/*2. feladat
-Hozd létre az órarend programot, mely a heti órarend adatait tartalmazza.
-Az adatokat tömbben és objectben tárold.
-Írask ki a heti órarendet. */
-
-const hours = ['8:00 - ', '8:55 - ', '9:55 - ', '10:50 - ', '11:45 - ', '12:45 - ', '13:40 - '];
-let timetable = {
-    'Hétfő': [],
-    'Kedd': [],
-    'Szerda': [],
-    'Csütörtök': [],
-    'Péntek': []
-};
-
-const keys = Object.keys(timetable);
-
-for (let i = 0; i < keys.length; i++) {
-    timetable[keys[i]] = hours;
+const timeTable =
+{
+hetfo: [],
+kedd: [],
+szerda: [],
+csutortok: [],
+pentek: [],
 }
-
-// Kérjen be a felhasználótól az órarend adatait inputtal
-for (let day in timetable) {
-    console.log(day);
-    for (let i = 0; i < timetable[day].length; i++) {
-        timetable[day][i] = input(`Add meg a(z) ${day} ${i + 1}. óráját: `);
+let ora = "";
+let numberOfHours = 0;
+for(let key in timeTable)
+{
+    numberOfHours = Math.abs(parseInt(await input(`Kérem a(z) ${key} nap óraszámát: `)))
+    for(let i = 0; i < numberOfHours; i++)
+    {
+        ora = await input(`${i+1}. óra neve > `)
+        timeTable[key].push(ora)
     }
 }
 
-
-for (let day in timetable) {
-    console.log(day);
-    for (let hour of timetable[day]) {
-        console.log(`\t${hour}`);
+for(let key in timeTable)
+    {
+        let hours = timeTable[key]
+        console.log(`${key}`)
+        for(let i = 1; i <= timeTable[key].length; i++)
+        {
+           console.log(`${i}. ${hours[i-1]} `)
+        }
     }
-}
