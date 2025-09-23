4a1aaed9d311ea5f9f37b3c7db7b5ca1c14af362
igen <nem>
2025-09-23 11:13:06 +0200

    2025. 09. 23. - Refactor birthday song input handling to remove nullability check

--- .dart/outerloop-vagy-idkxd/birthdaySong.dart
+++ .dart/outerloop-vagy-idkxd/birthdaySong.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 void main() {
     print("Adjon meg egy számot 1 és 100 között:");
-    int? szam = int.tryParse(stdin.readLineSync() ?? '');
+    int szam = int.tryParse(stdin.readLineSync() ?? "") ?? 1;
     
-    if (szam != null && szam >= 1 && szam <= 100) {
+    if (szam >= 1 && szam <= 100) {
         for (int i = 0; i < szam; i++) {
             print("Happy birthday!");
             print("Happy birthday to you!");
