ead368d3f7fb331056605d089a5b1450317222c6
HelloandMore <milan.kondor06@gmail.com>
2026-01-27 12:59:15 +0100

    2026. 01. 27. - battery ran out of power, almost done

--- .basic_gyakorlas/random_courses/app.js
+++ .basic_gyakorlas/random_courses/app.js
@@ -1,8 +1,39 @@
-import express from 'express';
-import db from './data/db';
+import express from "express";
+import db from "./data/db.js";
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+
+app.get("/students/:id", (req, res) => {
+  const { id } = req.params;
+  const stmt = db.prepare("SELECT * FROM `class-members` WHERE id = ?");
+  const student = stmt.get(id);
+  if (student) {
+    res.status(200).json(student);
+  } else {
+    res.status(404).json({ error: "Student not found" });
+  }
+});
+
+app.get("/subjects", (req, res) => {
+  const stmt = db.prepare("SELECT * FROM subjects");
+  const subjects = stmt.all();
+  res.status(200).json(subjects.sort((a, b) => a.name.localeCompare(b.name)));
+});
+
+app.get("/students", (req, res) => {
+  const stmt = db.prepare("SELECT * FROM `class-members`");
+  const students = stmt.all();
+  res
+    .status(200)
+    .json(
+      students
+        .sort((a, b) => (a.lastname || "").localeCompare(b.lastname || ""))
+        .sort((a, b) => (a.firstname || "").localeCompare(b.firstname || "")),
+    );
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
